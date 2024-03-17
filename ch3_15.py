a5f6f74a275f45ddb5be847e946b555ff8af1bc3
Jeonmijeong <162522454+Jeonmijeong@users.noreply.github.com>
2024-03-17 14:59:54 +0900

    Create ch3_15.py

---
+++ ch3_15.py
@@ -0,0 +1,10 @@
+# 15. 다음과 같은 결과가 나오도록 코드의 빈 곳을 채우세요.
+num = 12345678
+hex_num = hex(num)
+oct_num = oct(num)
+bin_num = bin(num) 
+
+print("10진수 ==> ", num)
+print("16진수 ==> ", hex_num)
+print("8진수 ==> ", oct_num)
+print("2진수 ==> ", bin_num)
