b35610b9972b3276aec770caf0e219a66f922fd2
Jeonmijeong <162522454+Jeonmijeong@users.noreply.github.com>
2024-03-17 14:59:15 +0900

    Create ch3_11.py

---
+++ ch3_11.py
@@ -0,0 +1,8 @@
+# 11. 다음 각 진수를 모두 10진수로 변환하세요.
+bin_num = "1011"
+num = int(bin_num, 2)
+print("2진수 ", bin_num, "→ 10진수", num)
+
+hex_num = "1A"
+num = int(hex_num, 16)
+print("16진수 ", hex_num, "→ 10진수", num)
