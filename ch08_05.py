58c54d6a66cca0fc266b9e6f9869ff07a261f3f7
Jeonmijeong <162522454+Jeonmijeong@users.noreply.github.com>
2024-04-01 17:58:00 +0900

    Delete ch08_05.py

--- ch08_05.py
+++
@@ -1,8 +0,0 @@
-# 5. 문자열의 중간마다 $를 끼워 넣는 코드입니다. 빈칸을 채우세요.
-ss = 'Python'
-
-for i in range(len(ss)):
-    if i != len(ss)-1:
-        print(ss[i]+'$', end='')
-    else:
-        print(ss[i], end='')
